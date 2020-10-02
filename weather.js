e40eec80103f19777d9e969b5f668208854482a3
gouz7514 <gouz7514@gmail.com>
2020-10-02 18:20:47 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const weather = document.querySelector(".js-weather");
 
 const API_KEY = "my_api_key";
-const COORDS = "coords"
+const COORDS = "coords";
 
 function getWeather(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
